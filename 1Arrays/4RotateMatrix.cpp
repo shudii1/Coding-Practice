ba997bb765daeb9b08060837b2af7f31d29a32b7
shudii1 <hustle162@gmail.com>
2021-02-01 13:47:11 +0530

    Initial commit

--- 1Arrays/4RotateMatrix.cpp
+++ 1Arrays/4RotateMatrix.cpp
@@ -1,8 +1,27 @@
 
 #include<iostream>
 using namespace std;
-#include "Solution.h"
+
 //void rotate(int **input, int n){
+
+void rotate(int **input, int n){
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Taking input and printing output is handled automatically.
+     */
+    int temp;
+    for(int i = 0; i < n/2; i++) {
+        for(int j = i; j < n-i-1; j++) {
+            temp=input[i][j];
+            input[i][j]=input[j][n-i-1];
+            input[j][n-i-1]=input[n-i-1][n-j-1];
+            input[n-i-1][n-j-1]=input[n-j-1][i];
+            input[n-j-1][i]=temp;
+
+        }
+    }
+}
+
 int main() {
     int n;
     cin >> n;
